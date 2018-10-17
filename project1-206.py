b24ad0f84bb8004332899cb43e20172472fadf23
Harrison <harrhall@umich.edu>
2018-10-16 20:14:34 -0400

    Most of the code done

--- project1-206.py
+++ project1-206.py
@@ -26,53 +26,113 @@ def getData(file):
 	inFile.close()
 	del dictList[0] # Deletes the first element, which was just the column headers
 	return dictList
+
 # get a list of dictionary objects from the file
 #Input: file name
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
 
+
 def mySort(data,col):
-# Sort based on key/column
-#Input: list of dictionaries and col (key) to sort on
-#Output: Return the first item in the sorted list as a string of just: firstName lastName
-
-	pass
-
+	sortedList = sorted(data, key=lambda k: k[col])
+	x = sortedList[0]
+	return (x["First"] + " " + x["Last"])
 
 def classSizes(data):
+	num_sr = 0
+	num_jr = 0
+	num_so = 0
+	num_fr = 0
+	for i in data:
+		if i['Class'] == 'Senior':
+			num_sr += 1
+		elif i['Class'] == 'Junior':
+			num_jr += 1
+		elif i['Class'] == 'Sophomore':
+			num_so += 1
+		elif i['Class'] == 'Freshman':
+			num_fr += 1
+	l = [("Senior",num_sr), ("Junior",num_jr), ("Sophomore",num_so), ("Freshman",num_fr)]
+	return sorted(l, key=lambda k: k[1], reverse = True)
+
 # Create a histogram
 # Input: list of dictionaries
 # Output: Return a list of tuples sorted by the number of students in that class in
 # descending order
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
 
-	pass
-
 
 def findMonth(a):
-# Find the most common birth month form this data
+	num_1 = 0
+	num_2 = 0
+	num_3 = 0
+	num_4 = 0
+	num_5 = 0
+	num_6 = 0
+	num_7 = 0
+	num_8 = 0
+	num_9 = 0
+	num_10 = 0
+	num_11 = 0
+	num_12 = 0
+	for i in a:
+		val = i['DOB'].split('/')[0] #splits the key for DOB of each dictionary at the slash
+		if val == '1':
+			num_1 += 1
+		elif val == '2':
+			num_2 += 1
+		elif val == '3':
+			num_3 += 1
+		elif val == '4':
+			num_4 += 1
+		elif val == '5':
+			num_5 += 1
+		elif val == '6':
+			num_6 += 1
+		elif val == '7':
+			num_7 += 1
+		elif val == '8':
+			num_8 += 1
+		elif val == '9':
+			num_9 += 1
+		elif val == '10':
+			num_10 += 1
+		elif val == '11':
+			num_11 += 1
+		elif val == '12':
+			num_12 += 1
+	new_l = [(1,num_1),(2,num_2),(3,num_3),(4,num_4),(5,num_5),(6,num_6),(7,num_7),(8,num_8),(9,num_9),(10,num_10),(11,num_11),(12,num_12)]
+	new_l_2 = (sorted(new_l, key=lambda k: k[1], reverse = True)[0]) #sort the list by values
+	return new_l_2[0] #return the month for the highest value tuple
+# Find the most common birth month from this data
 # Input: list of dictionaries
 # Output: Return the month (1-12) that had the most births in the data
 
-	pass
-
 def mySortPrint(a,col,fileName):
-#Similar to mySort, but instead of returning single
-#Student, the sorted data is saved to a csv file.
-# as fist,last,email
-#Input: list of dictionaries, col (key) to sort by and output file name
-#Output: No return value, but the file is written
-
-	pass
+	x = open(fileName, 'w')
+	sortedList = sorted(a, key=lambda k: k[col])
+	for i in sortedList:
+		x.write(str(i["First"] + " " + i["Last"] + " " + i["Email"] + "\n"))
+	x.close()
 
 def findAge(a):
-# def findAge(a):
-# Input: list of dictionaries
-# Output: Return the average age of the students and round that age to the nearest
-# integer.  You will need to work with the DOB and the current date to find the current
-# age in years.
-
-	pass
+	birthList = []
+	newList = []
+	ageList = []
+	finalList = []
+	counter = 0
+	for i in a: #loop through input, add DOBs to birthList
+		birthList.append(i["DOB"][:-1]) #add DOB to birthList but dont include the newline character
+	for x in birthList: #loop through birthList, split at '/'
+		newList.append(x.split("/")) #add the split births to newList
+	for y in newList:
+		ageList.append(date.today() - date(int(y[2]), int(y[0]), int(y[1]))) #subtracts birth date from today's date
+	for z in ageList:
+		finalList.append(z / timedelta(days = 365))
+	for a in finalList:
+		counter += a #adds the ages (in floats of years) together
+	counter = counter / len(finalList) #divides that number by the number of elements (people)
+	return round(counter) #returns that average rounded to the nearest int
 
 
 ################################################################
